f65272a0e47f80da8e1c85523c71032b5f023fa3
Alex Broadhead <21ABroadhead@heckgrammar.co.uk>
2024-06-26 13:39:30 +0100

    De bugged

--- Program.cs
+++ Program.cs
@@ -10,15 +10,19 @@ namespace Y9_DEC_TO_BIN_SKELETON
     {
         static void Main(string[] args)
         {
-            //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
-            int myInt = 0; //just for testing single stepping
-            string myString = "12"; //watch me being cast from string to int
-            int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
-            string myIntAsString = myInt.ToString();
-            myString = myString + myIntAsString;
+            ////STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
+            //int myInt = 0; //just for testing single stepping
+            //string myString = "12"; //watch me being cast from string to int
+            //int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
+            //string myIntAsString = myInt.ToString();
+            //myString = myString + myIntAsString;
             //MAIN:  NUMBER CONVERSION PROGRAM
-            Console.WriteLine("Enter your number from 0 to 255: ");
-            int usernum = Convert.ToInt32(Console.ReadLine());
+            int usernum = 256;
+            while (usernum <= -1 || usernum >= 256)
+            {
+                Console.WriteLine("Enter your number from 0 to 255: ");
+                usernum = Convert.ToInt32(Console.ReadLine());
+            }
             string[] BinaryNum = { "0", "0", "0", "0", "0", "0", "0", "0" };
             for (int i = 0; i < BinaryNum.Length; i++)
             {
@@ -27,7 +31,6 @@ namespace Y9_DEC_TO_BIN_SKELETON
                 usernum = usernum / 2;
             }
             Console.WriteLine(BinaryNum[0] + BinaryNum[1] + BinaryNum[2] + BinaryNum[3] + BinaryNum[4] + BinaryNum[5] + BinaryNum[6] + BinaryNum[7]);
-            //CODE GOES HERE
         }
 
         //static void means the function will not return a value so it does not need a data type 
